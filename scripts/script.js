b53f5ebf70f788032fcdf48655ada94256a1e504
nikotheflow <bboyfoxxx@yandex.ru>
2021-12-09 18:15:49 +0300

    add buttons activity check

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let position = 0;
-const slidesToScroll = 2;
+const slidesToScroll = 3;
 const slidesToShow = 3;
 
 const btnNext = document.querySelector('.btn-next');
@@ -14,20 +14,20 @@ let slidesHided = 0;
 let slidesLeft = sliderItemsCount - slidesToShow;
 let slideWidth = window.getComputedStyle(sliderItems[0]).width.match(/\d*/)[0];
 
-btnNext.addEventListener('click', () => {
-  countSlides();
-  
+btnNext.addEventListener('click', () => {  
   if (slidesLeft < slidesToScroll) {
-    position -= Math.abs(slidesLeft * slideWidth);
+    position -= Math.abs(slidesLeft * slideWidth);    
   } else {
     position -= Math.abs(slidesToScroll * slideWidth);
   }
   
-  sliderTrack.style.transform = `translateX(${position}px)`; 
-});
+  sliderTrack.style.transform = `translateX(${position}px)`;
+
+  countSlides();
+  console.log(slidesLeft, slidesHided)
+})
 
 btnPrev.addEventListener('click', () => {
-  countSlides();
 
   if (slidesHided < slidesToScroll) {
     position += Math.abs(slidesHided * slideWidth);
@@ -36,9 +36,14 @@ btnPrev.addEventListener('click', () => {
   }
   
   sliderTrack.style.transform = `translateX(${position}px)`;
-});
+
+  countSlides();
+})
 
 function countSlides() {
   slidesHided = Math.abs(position / slideWidth);
-  slidesLeft = sliderItemsCount - slidesToShow - slidesHided;  
+  slidesLeft = sliderItemsCount - slidesToShow - slidesHided;
+
+  btnPrev.disabled = (slidesHided == 0 ? true : false);
+  btnNext.disabled = (slidesLeft == 0 ? true : false);
 }
