a9139112ba7e957b986da830e4a213ba0a18435e
nikotheflow <bboyfoxxx@yandex.ru>
2021-12-13 22:16:51 +0300

    add responsive for different number of slides

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // === VARIABLES ===
 
 let position = 0;
-const slidesToScroll = 1;
-const slidesToShow = 1;
+const slidesToScroll = 3;
+const slidesToShow = 3;
 
 const wrapper = document.querySelector('.wrapper');
 const sliderItems = document.querySelectorAll('.slider-item');
@@ -16,6 +16,7 @@ const sliderItemsCount = sliderItems.length;
 let slidesHided = 0;
 let slidesLeft = sliderItemsCount - slidesToShow;
 let slideWidth = window.getComputedStyle(sliderItems[0]).width.match(/\d*/)[0];
+let slideDistance = Number(window.getComputedStyle(sliderItems[0]).marginRight.match(/\d*/)[0]);
 
 let positionStart = 'unclick';
 let delPosition = 0;
@@ -23,13 +24,20 @@ let delPosition = 0;
 // === / VARIABLES ===
 
 
+// === COMMONS ===
+
+setParameters(340);
+
+// === / COMMONS ===
+
+
 // === BUTTONS ===
 
 btnNext.addEventListener('click', () => {
   if (slidesLeft < slidesToScroll) {
-    position -= Math.abs(slidesLeft * slideWidth);    
+    position = -Math.abs((sliderItemsCount - slidesToShow) * slideWidth + (sliderItemsCount - slidesToShow) * slideDistance);
   } else {
-    position -= Math.abs(slidesToScroll * slideWidth);
+    position -= Math.abs(slidesToScroll * (Number(slideWidth) + Number(slideDistance)));
   }
     
   moveSlides();
@@ -38,9 +46,9 @@ btnNext.addEventListener('click', () => {
 
 btnPrev.addEventListener('click', () => {
   if (slidesHided < slidesToScroll) {
-    position += Math.abs(slidesHided * slideWidth);
+    position = 0;
   } else {
-    position += Math.abs(slidesToScroll * slideWidth);
+    position += Math.abs(slidesToScroll * (Number(slideWidth) + Number(slideDistance)));
   }
     
   moveSlides();
@@ -64,6 +72,9 @@ sliderContainer.addEventListener('mouseup', mouseUpSwipe);
 
 
 // === FUNCTIONS ===
+function setParameters(wrapperWidth, slideWidth) {  
+  console.log(wrapper.style.width = `${wrapperWidth}px`);
+}
 
 function moveSlides() {
   sliderTrack.style.transform = `translateX(${position}px)`;
@@ -71,7 +82,7 @@ function moveSlides() {
 }
 
 function countSlides() {
-  slidesHided = Math.abs((position) / slideWidth);
+  slidesHided = Math.abs(position / (Number(slideWidth) + Number(slideDistance)));
   slidesLeft = sliderItemsCount - slidesToShow - slidesHided;
   
   btnPrev.disabled = (slidesHided == 0 ? true : false);
@@ -86,31 +97,29 @@ function mouseMoveSwipe() {
     delPosition = positionCurrent - positionStart;
 
     sliderTrack.style.transform = `translateX(${(position + delPosition)}px)`;
-  }
-  
+  }  
 }
 
 function correctPosition() {
   positionStart = 'unclick';  
   position += delPosition;  
 
-  if (Math.abs(delPosition) < slideWidth / 2) {
-    position = -slidesHided * slideWidth;
-  } else if (delPosition < - slideWidth / 2) {
-    position = (slidesHided + 1) * -slideWidth;  
-  } else if (delPosition > slideWidth / 2) {
-    position = (slidesHided - 1) * -slideWidth;    
+  if (Math.abs(delPosition) <= slideWidth / 3) {
+    position = -slidesHided * (Number(slideWidth) + Number(slideDistance));
+  } else if (delPosition < - slideWidth / 3) {
+    position = (slidesHided + 1) * (-slideWidth - Number(slideDistance)) * slidesToScroll;
+  } else if (delPosition > slideWidth / 3) {
+    position = (slidesHided - 1) * (-slideWidth - Number(slideDistance)) ;
   }
 
   if (position > 0) {
     position = 0;
-  } else if (position < -(sliderItemsCount - 1) * slideWidth) {
-    position = -(sliderItemsCount - 1) * slideWidth;
+  } else if (position < -(sliderItemsCount - slidesToShow) * (Number(slideWidth) + Number(slideDistance))) {
+    position = -(sliderItemsCount - slidesToShow) * (Number(slideWidth) + Number(slideDistance));
   }
 }
 
 function mouseDownSwipe() {
-  console.log(event);
   positionStart = event.clientX;
 }
 
